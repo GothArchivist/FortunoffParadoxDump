ec976bbbc9a1906aaa2514fd5daf67c1b9ee838c
Christy <christy.tomecek@yale.edu>
2018-08-21 16:34:22 -0400

    Update GeneralInfoForm2.sql

--- GeneralInfoForm2.sql
+++ GeneralInfoForm2.sql
@@ -1,5 +1,6 @@
-SELECT c.Barcode, c.`Recording type`, c.`T-number`, c.`Tape format`, c.`Shared with`, c.`Created from`, c.`Creation date` AS RecordingDate, c.`Stock manufacturer`, c.PermanentLocation, 
-c.TemporaryLocation AS ItemLocation, reststat.`Restoration status`, c.RestorationCode, c.`Tape wind`, c.Odor, c.`Edge damage`, c.Notes 
+SELECT c.Barcode, c.`Recording type`, c.`T-number`, c.`Tape format`, c.`Shared with`, c.`Created from`, c.`Creation date` AS RecordingDate, c.`Stock manufacturer`, 
+c.TemporaryLocation, location.Location AS BoxLocation, c.PermanentLocation AS ItemLocation, reststat.`Restoration status`, c.RestorationCode, c.`Tape wind`, c.Odor, c.`Edge damage`, c.Notes 
 FROM cassette c
+LEFT JOIN location ON c.PermanentLocation=location.Container
 LEFT JOIN reststat ON c.RestorationCode=reststat.Code
-WHERE c.`T-number`=#Put in T-number
+WHERE c.`T-number`=#Put in T number
