9cb7c394d1e537366cd4b6436218b77001884c48
Christy <christy.tomecek@yale.edu>
2018-08-23 16:30:49 -0400

    Update ContainerForm.sql

--- ContainerForm.sql
+++ ContainerForm.sql
@@ -1,3 +1,5 @@
+#DO NOT GET RID OF THE ACCENT MARKS AROUND COLUMNS (e.g. `T-number`). Paradox is the devil and it needs those marks to not mess up in a standard MySQL database.
+#For cassette.`T-number`, you do not need quotation numbers around the number. Properly filled out, it should look like 'WHERE cassette.`T-number`=45.
 SELECT cassette.`T-number`, location.Container, location.Location, cassette.`Recording type`, cassette.`Cassette number`, cassette.Barcode
 FROM cassette
 LEFT JOIN location ON cassette.PermanentLocation=location.Container
