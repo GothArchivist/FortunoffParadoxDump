fce7814904c5e183c8a094938a1dada3a11ec3dd
Christy <christy.tomecek@yale.edu>
2018-08-23 16:34:37 -0400

    Update CassettesForm.sql

--- CassettesForm.sql
+++ CassettesForm.sql
@@ -1,5 +1,5 @@
 #DO NOT GET RID OF THE ACCENT MARKS AROUND COLUMNS (e.g. `T-number`). Paradox is the devil and it needs those marks to not mess up in a standard MySQL database.
-#For c.`T-number`, you do not need quotation numbers around the number. Properly filled out, it should look like 'WHERE cassette.`T-number`=45.
+#For c.`T-number`, you do not need quotation numbers around the number. Properly filled out, it should look like 'WHERE c.`T-number`=45.
 SELECT c.Barcode, c.`Recording type`, c.`T-number`, c.`Tape format`, c.`Shared with`, c.`Created from`, c.`Creation date` AS RecordingDate, c.`Stock manufacturer`, 
 c.TemporaryLocation, location.Location AS BoxLocation, c.PermanentLocation AS ItemLocation, reststat.`Restoration status`, c.RestorationCode, c.`Tape wind`, c.Odor, c.`Edge damage`, c.Notes 
 FROM cassette c
