145a565a06cf66595d8018f219ac9161c9c86539
Christy <christy.tomecek@yale.edu>
2018-08-23 16:30:26 -0400

    Update CatalogingForm.sql

--- CatalogingForm.sql
+++ CatalogingForm.sql
@@ -1,2 +1,4 @@
+#DO NOT GET RID OF THE ACCENT MARKS AROUND COLUMNS (e.g. `T-number`). Paradox is the devil and it needs those marks to not mess up in a standard MySQL database.
+#For cassette.`T-number`, you do not need quotation numbers around the number. Properly filled out, it should look like 'WHERE cassette.`T-number`=45.
 SELECT * FROM process
 WHERE `T-number`=#put in the T number
